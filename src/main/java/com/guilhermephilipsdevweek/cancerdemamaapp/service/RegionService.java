75468995b7a8eb0960d5e6115b4d5747613cdec1
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-03-10 19:35:33 -0300

    Implements Region Service Layer & List All HTTP method

--- src/main/java/com/guilhermephilipsdevweek/cancerdemamaapp/service/RegionService.java
+++ src/main/java/com/guilhermephilipsdevweek/cancerdemamaapp/service/RegionService.java
@@ -1,13 +1,19 @@
 package com.guilhermephilipsdevweek.cancerdemamaapp.service;
 
+import com.guilhermephilipsdevweek.cancerdemamaapp.entity.Region;
 import com.guilhermephilipsdevweek.cancerdemamaapp.repository.RegionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
-public class RegiaoService {
+public class RegionService {
 
-    private final RegionRepository regiaoRepository;
+    private final RegionRepository regionRepository;
 
+    public List<Region> listAll() {
+        return regionRepository.findAll();
+    }
 }
