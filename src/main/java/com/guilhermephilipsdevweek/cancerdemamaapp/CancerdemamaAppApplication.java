e2e7816be93f3122fa961f89981a2b4572f5fd29
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-03-10 19:02:24 -0300

    Initial commit

---
+++ src/main/java/com/guilhermephilipsdevweek/cancerdemamaapp/CancerdemamaAppApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermephilipsdevweek.cancerdemamaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancerdemamaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancerdemamaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermephilipsdevweek/cancerdemamaapp/CancerdemamaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermephilipsdevweek.cancerdemamaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancerdemamaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
